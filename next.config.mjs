d990df637da1e69902e4bcf22e35075e9db406d7
Felipe <felipe.austriaco@mercadolivre.com>
2024-02-01 15:53:41 -0300

    chore: :wrench: add next-pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        hostname: 'github.com',
-      },
-    ],
-  },
-}
+import withPWAInit from '@ducanh2912/next-pwa'
 
-export default nextConfig
+const withPWA = withPWAInit({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === "development",
+  // register: true,
+  // scope: "/app",
+  // sw: "service-worker.js",
+  // ...
+})
+
+// Your Next config is automatically typed!
+export default withPWA({
+  // Your Next.js config
+})
